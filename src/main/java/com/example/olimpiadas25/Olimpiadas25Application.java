4959fef1f44525eebe39b048f00d6248a7c514c2
Lucas Pulido <lucas.a.pulido@gmail.com>
2025-06-09 16:33:43 -0300

    Se agrega la base

---
+++ src/main/java/com/example/olimpiadas25/Olimpiadas25Application.java
@@ -0,0 +1,13 @@
+package com.example.olimpiadas25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Olimpiadas25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Olimpiadas25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/olimpiadas25/Olimpiadas25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olimpiadas25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Olimpiadas25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
